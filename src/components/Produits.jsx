7aa39de0e89adca205a70e989449917fcbe4d9d6
ahafid78 <118371686+ahafid78@users.noreply.github.com>
2023-07-25 16:03:26 +0100

    style

--- src/components/Produits.jsx
+++ src/components/Produits.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Produits() {
   return (
     <div>
-       <h1>  Nos Produits </h1>
+       <h1> Nos Produits </h1>
        <h2>Particuliers</h2>
        <h2>Professionnels</h2>
-       <h2>Entreprises</h2>  
+        
     </div>
   )
 }
