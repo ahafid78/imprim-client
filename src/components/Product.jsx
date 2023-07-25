e6d2f926efc3fac7fc4f14cc5503f1266753fda6
ahafid78 <118371686+ahafid78@users.noreply.github.com>
2023-07-25 14:59:55 +0100

    style

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,9 +8,11 @@ export default function Product({ product }) {
                 <div className="descimg"><img  src={product.image} alt={product.title} width={200} height={200} /></div>
                 
                 <div><h3 ><span className="detitle"> {product.title} </span> </h3></div>
-
+                <div>
+                <p><span className="description"> {product.description} </span></p>
+                </div>
                 <div  style={{ display: 'flex' }}>
-                    <p><span className="description"> {product.description} </span></p>
+                   
 
                     <h2> <span className="price"> {product.price} £ </span> </h2>
 
